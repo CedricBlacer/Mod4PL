e88301978ab39b96e9e1067f20f7d592426e42c3
jameson3081 <jamesonlouis.celzo.cics@ust.edu.ph>
2022-09-22 12:56:37 +0800

    Added Routes for added EJS views

--- app.js
+++ app.js
@@ -11,9 +11,70 @@ app.get("/", (req,res) => {
     res.render('index', {title:'Simply Tasty | Home'})
 })
 
+app.get("/Account", (req,res) => {
+    res.render('Account', {title:'Account'})
+})
+
+app.get("/Cart", (req,res) => {
+    res.render('Cart', {title:'Your Cart'})
+})
+
+app.get("/Checkout", (req,res) => {
+    res.render('Checkout', {title:'Finalize Your Orders'})
+})
+
+app.get("/ContactAndShippingDetails", (req,res) => {
+    res.render('ContactAndShippingDetails', {title:'Enter Contact'})
+})
+
+app.get("/CreateAccount", (req,res) => {
+    res.render('CreateAccount', {title:'Register Your Account'})
+})
+
+app.get("/Desserts", (req,res) => {
+    res.render('Desserts', {title:'Choose Your Desserts'})
+})
+
+app.get("/EnterNewPassword", (req,res) => {
+    res.render('EnterNewPassword', {title:'Create Password'})
+})
+
+app.get("/ForgotPassword", (req,res) => {
+    res.render('ForgotPassword', {title:'Forgot Password'})
+})
+
+app.get("/Meals", (req,res) => {
+    res.render('Meals', {title:'Main Dishes'})
+})
+
 app.get("/OurMenu", (req,res) => {
     res.render('OurMenu', {title:'Choose Your Food Category'})
 })
+
+app.get("/PaymentDetails", (req,res) => {
+    res.render('PaymentDetails', {title:'Payment Details'})
+})
+
+app.get("/PlaceOrder", (req,res) => {
+    res.render('PlaceOrder', {title:'Place Your Order'})
+})
+
+app.get("/ResetPassword", (req,res) => {
+    res.render('ResetPassword', {title:'Reset Your Password'})
+})
+
+app.get("/SendEmail", (req,res) => {
+    res.render('SendEmail', {title:'Send Email'})
+})
+
+app.get("/Signup", (req,res) => {
+    res.render('Signup', {title:'Register'})
+})
+
+app.get("/Snacks", (req,res) => {
+    res.render('Snacks', {title:'Snacks'})
+})
+
 
 app.use((req, res) =>{
     res.status(404).render('404', {title: 'Page not Found'})
