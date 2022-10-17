99c70b986b1a7279baad63088122afa2aeb9b2ba
jameson3081 <jamesonlouis.celzo.cics@ust.edu.ph>
2022-10-17 21:06:20 +0800

    exported ROUTES to pages.js
    Decluttered app.js

--- app.js
+++ app.js
@@ -8,79 +8,12 @@ const path =require ('path');
 
 //import auth routes
 const AuthRoute = require('./routes/auth')
+const pagesRoute = require('./routes/pages')
 
 //register view engine EJS
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 
-//ROUTES
-app.get("/", (req,res) => {
-    res.render('index', {title:'Simply Tasty | Home'})
-})
-
-app.get("/Account", (req,res) => {
-    res.render('Account', {title:'Account'})
-})
-
-app.get("/Cart", (req,res) => {
-    res.render('Cart', {title:'Your Cart'})
-})
-
-app.get("/Checkout", (req,res) => {
-    res.render('Checkout', {title:'Finalize Your Orders'})
-})
-
-app.get("/ContanctAndShippingDetails", (req,res) => {
-    res.render('ContanctAndShippingDetails', {title:'Enter Contact'})
-})
-
-app.get("/CreateAccount", (req,res) => {
-    res.render('CreateAccount', {title:'Register Your Account'})
-})
-
-app.get("/Desserts", (req,res) => {
-    res.render('Desserts', {title:'Choose Your Desserts'})
-})
-
-app.get("/EnterNewPassword", (req,res) => {
-    res.render('EnterNewPassword', {title:'Create Password'})
-})
-
-app.get("/ForgotPassword", (req,res) => {
-    res.render('ForgotPassword', {title:'Forgot Password'})
-})
-
-app.get("/Meals", (req,res) => {
-    res.render('Meals', {title:'Main Dishes'})
-})
-
-app.get("/OurMenu", (req,res) => {
-    res.render('OurMenu', {title:'Choose Your Food Category'})
-})
-
-app.get("/PaymentDetails", (req,res) => {
-    res.render('PaymentDetails', {title:'Payment Details'})
-})
-
-app.get("/PlaceOrder", (req,res) => {
-    res.render('PlaceOrder', {title:'Place Your Order'})
-})
-
-app.get("/ResetPassword", (req,res) => {
-    res.render('ResetPassword', {title:'Reset Your Password'})
-})
-
-app.get("/SendEmail", (req,res) => {
-    res.render('SendEmail', {title:'Send Email'})
-})
-
-app.get("/Signup", (req,res) => {
-    res.render('Signup', {title:'Register'})
-})
-
-app.get("/Snacks", (req,res) => {
-    res.render('Snacks', {title:'Snacks'})
-})
 
 //ESTABLISHING MONGODB NODESERVER CONNECTION FOR LOGIN/REGISTER SYSTEM
 //where 'test' is the name of the db in mongoose
@@ -101,15 +34,12 @@ app.use(bodyParser.json())
 // END OF ESTABLISHING MONGODB CONNECTION
 
 
-
-
-
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>{
     console.log(`Server running at port ${PORT}`)
 })
 
-//USE API ROUTE
-app.use('/api', AuthRoute)
+
+app.use('/api', AuthRoute) //USE API ROUTE
+app.use('/', pagesRoute) //USE PAGES ROUTE
---
+++ routes/pages.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const router = express.Router()
+
+
+//ROUTES
+router.get("/", (req,res) => {
+    res.render('index', {title:'Simply Tasty | Home'})
+})
+
+router.get("/Account", (req,res) => {
+    res.render('Account', {title:'Account'})
+})
+
+router.get("/Cart", (req,res) => {
+    res.render('Cart', {title:'Your Cart'})
+})
+
+router.get("/Checkout", (req,res) => {
+    res.render('Checkout', {title:'Finalize Your Orders'})
+})
+
+router.get("/ContanctAndShippingDetails", (req,res) => {
+    res.render('ContanctAndShippingDetails', {title:'Enter Contact'})
+})
+
+router.get("/CreateAccount", (req,res) => {
+    res.render('CreateAccount', {title:'Register Your Account'})
+})
+
+router.get("/Desserts", (req,res) => {
+    res.render('Desserts', {title:'Choose Your Desserts'})
+})
+
+router.get("/EnterNewPassword", (req,res) => {
+    res.render('EnterNewPassword', {title:'Create Password'})
+})
+
+router.get("/ForgotPassword", (req,res) => {
+    res.render('ForgotPassword', {title:'Forgot Password'})
+})
+
+router.get("/Meals", (req,res) => {
+    res.render('Meals', {title:'Main Dishes'})
+})
+
+router.get("/OurMenu", (req,res) => {
+    res.render('OurMenu', {title:'Choose Your Food Category'})
+})
+
+router.get("/PaymentDetails", (req,res) => {
+    res.render('PaymentDetails', {title:'Payment Details'})
+})
+
+router.get("/PlaceOrder", (req,res) => {
+    res.render('PlaceOrder', {title:'Place Your Order'})
+})
+
+router.get("/ResetPassword", (req,res) => {
+    res.render('ResetPassword', {title:'Reset Your Password'})
+})
+
+router.get("/SendEmail", (req,res) => {
+    res.render('SendEmail', {title:'Send Email'})
+})
+
+router.get("/Signup", (req,res) => {
+    res.render('Signup', {title:'Register'})
+})
+
+router.get("/Snacks", (req,res) => {
+    res.render('Snacks', {title:'Snacks'})
+})
+
+router.use((req, res) =>{
+    res.status(404).render('404', {title: 'Page not Found'})
+})
+
+
+module.exports = router
