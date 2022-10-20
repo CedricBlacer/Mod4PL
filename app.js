41f7a6117288041d0b1cb0aaee662db744b807a3
jameson3081 <jamesonlouis.celzo.cics@ust.edu.ph>
2022-10-20 11:09:37 +0800

    added mongoDB server connections
    dropped localhost connection

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express();
 const path =require ('path');
+
 
 
 //import auth routes
@@ -16,8 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 //ESTABLISHING MONGODB NODESERVER CONNECTION FOR LOGIN/REGISTER SYSTEM
-//where 'test' is the name of the db in mongoose
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true} )
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true} )
 const db = mongoose.connection
 
 db.on('error', (err) => {
@@ -25,7 +26,7 @@ db.on('error', (err) => {
 })
 
 db.once('open', () => {
-    console.log('Database Connection Established...')
+    console.log('Server Database Connection Established...')
 })
 
 app.use(morgan('dev'))
