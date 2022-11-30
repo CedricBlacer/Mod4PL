c7ff77e1236edb5812b1177f8572503bfd37c799
jameson3081 <jamesonlouis.celzo.cics@ust.edu.ph>
2022-11-30 17:26:40 +0800

    fixed comment

--- routes/auth.js
+++ routes/auth.js
@@ -7,9 +7,6 @@ const AuthController = require('../controllers/AuthController')
 router.get('/accounts', AuthController.listAccounts)
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
-
-//ISSUE NIYA UNG INPUTS DUN SA 'ENTERNEWPASSWORD' PAGE 
-//NAPAPASA NIYA DUN SA 'changePassword1' function
 router.post('/changePassword', AuthController.changePassword1)
 router.post('/changePassword2', AuthController.changePassword2)
 
